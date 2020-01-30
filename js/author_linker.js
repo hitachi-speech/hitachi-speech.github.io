aa0ab6cab4f22001cce31cd703183d488c5efc8e
Shota Horiguchi <shota.horiguchi.wk@hitachi.com>
2020-01-30 18:02:13 +0900

    [add] link to author pages

---
+++ js/author_linker.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var authors = document.getElementsByClassName("author");
+    for (var i = 0, len=authors.length|0; i < len; i = i + 1 | 0) {
+        authors[i].innerHTML = authors[i].innerHTML.replace("Masato Tamura", "<a href=\"https://scholar.google.co.jp/citations?user=IbPzCocAAAAJ&hl=en\">Masato Tamura</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Quan Kong", "<a href=\"https://scholar.google.co.uk/citations?user=9lqFOsEAAAAJ&hl=en\">Quan Kong</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Shota Horiguchi", "<a href=\"https://scholar.google.com/citations?user=9U5YK3wAAAAJ&hl=en\">Shota Horiguchi</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Yuki Inoue", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=ZemgttEAAAAJ\">Yusuke Fujita</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Yusuke Fujita", "<a href=\"https://scholar.google.com/citations?user=8e5X3BQAAAAJ&hl=en\">Yusuke Fujita</a>");
+    }
+})
