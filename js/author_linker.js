7255ca86cb311cbac509d40ee6de68fb2873c569
Shota Horiguchi <shota.horiguchi.wk@hitachi.com>
2020-01-31 12:07:37 +0900

    [update] add one person & fix typo

--- js/author_linker.js
+++ js/author_linker.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
         authors[i].innerHTML = authors[i].innerHTML.replace("Masato Tamura", "<a href=\"https://scholar.google.co.jp/citations?user=IbPzCocAAAAJ&hl=en\">Masato Tamura</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Quan Kong", "<a href=\"https://scholar.google.co.uk/citations?user=9lqFOsEAAAAJ&hl=en\">Quan Kong</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Shota Horiguchi", "<a href=\"https://scholar.google.com/citations?user=9U5YK3wAAAAJ&hl=en\">Shota Horiguchi</a>");
-        authors[i].innerHTML = authors[i].innerHTML.replace("Yuki Inoue", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=ZemgttEAAAAJ\">Yuki Inout</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Yuki Inoue", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=ZemgttEAAAAJ\">Yuki Inoue</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Yusuke Fujita", "<a href=\"https://scholar.google.com/citations?user=8e5X3BQAAAAJ&hl=en\">Yusuke Fujita</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Yuta Koreeda", "<a href=\"https://scholar.google.co.jp/citations?user=zUJ8Ao4AAAAJ&hl=en\">Yuta Koreeda</a>");
     }
 })
