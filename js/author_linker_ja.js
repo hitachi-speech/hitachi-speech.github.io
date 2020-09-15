3aeee873f802e58942563001f8b0c66dbc3289c0
Shota Horiguchi <shota.horiguchi.wk@hitachi.com>
2020-09-15 14:33:33 +0900

    [add] author link

--- js/author_linker_ja.js
+++ js/author_linker_ja.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
         authors[i].innerHTML = authors[i].innerHTML.replace("孔全", "<a href=\"https://scholar.google.co.uk/citations?user=9lqFOsEAAAAJ&hl=ja\">孔全</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("鴨志田亮太", "<a href=\"https://scholar.google.com/citations?hl=ja&user=HXJnKcEAAAAJ\">鴨志田亮太</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("堀口翔太", "<a href=\"https://scholar.google.com/citations?user=9U5YK3wAAAAJ&hl=ja\">堀口翔太</a>");
-        authors[i].innerHTML = authors[i].innerHTML.replace("川口洋平", "<a href=\"https://scholar.google.com/citations?user=W1HV9Q8AAAAJ&hl=en\">川口洋平</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("川口洋平", "<a href=\"https://scholar.google.com/citations?user=W1HV9Q8AAAAJ&hl=ja\">川口洋平</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("井上祐貴", "<a href=\"https://scholar.google.co.jp/citations?hl=ja&user=ZemgttEAAAAJ\">井上祐貴</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("藤田雄介", "<a href=\"https://scholar.google.com/citations?user=8e5X3BQAAAAJ&hl=ja\">藤田雄介</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("是枝裕太", "<a href=\"https://scholar.google.co.jp/citations?user=zUJ8Ao4AAAAJ&hl=ja\">是枝裕太</a>");
