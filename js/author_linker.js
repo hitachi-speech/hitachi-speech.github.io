734b04c7b65f4dd377ce00af6ca6691bd8e3cb80
Shota Horiguchi <shota.horiguchi.wk@hitachi.com>
2023-02-08 10:29:49 +0000

    [add] yawata

--- js/author_linker.js
+++ js/author_linker.js
@@ -1,14 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var authors = document.getElementsByClassName("author");
-   for (var i = 0, len=authors.length|0; i < len; i = i + 1 | 0) {
+    for (var i = 0, len = authors.length | 0; i < len; i = i + 1 | 0) {
         authors[i].innerHTML = authors[i].innerHTML.replace("Hiroki Ohashi", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=GKC6bbYAAAAJ\">Hiroki Ohashi</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Hiroaki Ozaki", "<a href=\"https://www.semanticscholar.org/author/Hiroaki-Ozaki/36904068\">Hiroaki Ozaki</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Katsuyuki Nakamura", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=ZIxQ5zAAAAAJ\">Katsuyuki Nakamura</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Koichiro Ito", "<a href=\"https://scholar.google.com/citations?user=hXr2Ez8AAAAJ&hl=en\">Koichiro Ito</a>");
+        authors[i].innerHTML = authors[i].innerHTML.replace("Koichiro Yawata", "<a href=\"https://scholar.google.com/citations?user=B0mNkbgAAAAJ&hl=en\">Koichiro Yawata</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Kota Dohi", "<a href=\"https://scholar.google.com/citations?hl=en&user=dTyWSqYAAAAJ\">Kota Dohi</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Masato Tamura", "<a href=\"https://scholar.google.co.jp/citations?user=IbPzCocAAAAJ&hl=en\">Masato Tamura</a>");
-        authors[i].innerHTML = authors[i].innerHTML.replace("Phong Nguyen", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=v3GPtFwAAAAJ\">Phong Nguyen</a>");
-        authors[i].innerHTML = authors[i].innerHTML.replace("Phong Xuan Nguyen", "<a href=\"https://scholar.google.co.jp/citations?hl=en&user=v3GPtFwAAAAJ\">Phong Xuan Nguyen</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Ryo Tanabe", "<a href=\"https://scholar.google.co.jp/citations?user=6anrT6wAAAAJ&hl=en\">Ryo Tanabe</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Ryota Kamoshida", "<a href=\"https://scholar.google.com/citations?hl=en&user=HXJnKcEAAAAJ\">Ryota Kamoshida</a>");
         authors[i].innerHTML = authors[i].innerHTML.replace("Shota Horiguchi", "<a href=\"https://scholar.google.com/citations?user=9U5YK3wAAAAJ&hl=en\">Shota Horiguchi</a>");
